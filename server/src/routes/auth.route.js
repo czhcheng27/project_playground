e15fb528e849c6c2f6a523ee1b6f52f4089fa817
Zihang Cheng <zihang.cheng@ott.ca>
2025-07-08 10:00:37 -0400

    initAdmin --- 暂时不需要了

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,9 +1,10 @@
 // 登录接口路由
 import express from "express";
-import { login } from "../controllers/auth.controller.js";
+import { login, logout } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/login", login);
+router.post("/logout", logout);
 
 export default router;
--- server/src/seeds/initAdmin.js
+++ server/src/seeds/initAdmin.js
@@ -1,6 +1,7 @@
-// src/seeds/initAdmin.js
+// 已在外部进行CI/CD设计，/scripts/initApp.ts进行了全部最新权限的录入及admin的初始化。但此文件先暂时保留
 import mongoose from "mongoose";
 import { User } from "../models/user.model.js";
+import { Permission } from "../models/permission.model.js";
 import { hashPassword } from "../lib/hash.js";
 
 async function init() {
@@ -9,6 +10,8 @@ async function init() {
     console.log("✅ Connected to MongoDB");
 
     const existing = await User.findOne({ username: "admin" });
+    // 获取全量权限
+    const allPermissions = await Permission.find({}, { route: 1, actions: 1 });
 
     if (existing) {
       console.log("ℹ️ Admin already exists.");
@@ -20,7 +23,7 @@ async function init() {
         password: hashed,
         email: "admin@email.com",
         roles: ["admin"],
-        permissions: ["user:create", "user:update", "user:delete"],
+        permissions: allPermissions,
       });
 
       console.log("✅ Admin user created: admin / admin");
