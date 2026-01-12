9cd87f18e5db45b996cf4f0fb73bb2653bf5dff7
zihang cheng <zihang.cheng@ott.ca>
2026-01-12 13:48:20 -0500

    Hover 3D Card component

---
+++ client/src/components/base/ThreeDCard/index.tsx
@@ -0,0 +1,31 @@
+import { type CSSProperties } from "react";
+import "./index.module..less";
+
+interface ThreeDCardProps {
+  children: React.ReactNode;
+  className?: string;
+  style?: CSSProperties;
+}
+
+const ThreeDCard = ({
+  children,
+  className = "",
+  style = {},
+}: ThreeDCardProps) => {
+  return (
+    <div className="hover-3d" style={style}>
+      <div className={className}>{children}</div>
+      {/* 8个透明的hover区域 (3x3网格，跳过中心)，用于检测鼠标位置 */}
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
+  );
+};
+
+export default ThreeDCard;
