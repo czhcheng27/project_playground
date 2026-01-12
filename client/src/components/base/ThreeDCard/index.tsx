3488663cbcc066b99ca0976be66cb7fadd41a4b9
zihang cheng <zihang.cheng@ott.ca>
2026-01-12 15:49:31 -0500

    update ThreeDCard: add onClick function

--- client/src/components/base/ThreeDCard/index.tsx
+++ client/src/components/base/ThreeDCard/index.tsx
@@ -5,15 +5,17 @@ interface ThreeDCardProps {
   children: React.ReactNode;
   className?: string;
   style?: CSSProperties;
+  onClick?: () => void;
 }
 
 const ThreeDCard = ({
   children,
   className = "",
   style = {},
+  onClick,
 }: ThreeDCardProps) => {
   return (
-    <div className="hover-3d" style={style}>
+    <div className="hover-3d" style={style} onClick={onClick}>
       <div className={className}>{children}</div>
       {/* 8个透明的hover区域 (3x3网格，跳过中心)，用于检测鼠标位置 */}
       <div />
